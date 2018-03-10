e3e628d1b61b856568320077293fbc1a3b65d7c7
suzie chen <suzie.chen@pcc.edu>
2018-03-10 14:29:44 -0800

    added display win screen

---
+++ display_win_screen.c
@@ -0,0 +1,15 @@
+//display win or loss screen
+#include <stdio.h>
+
+void diaplay_win (int the_winner){
+
+	if (the winner == '1')
+		printf("Congratulations!! You won!")
+
+	else {
+		printf("Sorry, you lost.")
+	}
+
+}
+
+
