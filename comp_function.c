34b3705609326b1c91cc4b709421e170516788ff
Alayal <37192197+Alayal@users.noreply.github.com>
2018-03-14 01:58:17 -0700

    Add files via upload

---
+++ comp_function.c
@@ -0,0 +1,90 @@
+// Layal A.
+// 3/13/2018
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define row 10
+#define col 11
+
+
+
+//random placement of ship
+char rand_ship(){
+
+  return "o o"[rand() % 5];
+  return "    "[rand() % 5];
+ 
+ 
+}
+
+//Main
+int main(){
+	srand(time(NULL));
+	disp_comp_board();
+	
+}
+
+
+void disp_comp_board(){
+
+//computer board
+ char comp_board[row][col]={
+								{'A'},
+								{'B'},
+								{'C'},
+								{'D'},
+								{'E'},
+								{'F'},
+								{'G'},
+								{'H'},
+								{'I'},
+								{'J'},		
+}; 
+ 
+//Row and Column Variables
+    int r =0;
+    int c =0;
+
+		printf("\n");		
+        printf("   | 1 | 2 | 3 | 4 | V | 6 | 7 | 8 | 9 | X | \n");
+        printf("--------------------------------------------\n");
+
+    while (c != 11){    // print column value, then advance c by 1 each loop until 11.
+    
+rand_ship != " ";
+
+//horizontal ships breaks between lines
+if(!comp_board[r][c]){
+int i;
+int o= "o";
+	for (o= 0; o <=i;o++){
+		if (rand_ship > 0){
+			--i;
+			comp_board[r][c]=0;
+			break;
+		}
+	}
+}
+
+  
+	
+		
+        printf("%c %c|", comp_board[r][c],rand_ship());
+		
+        c++;	
+                        // advances the row
+        if (c == 11){
+
+        printf("\n--------------------------------------------\n");
+
+        r++;
+
+        c = 0;
+        
+        }
+        if (r == 10){   // when row is 10 and Column is 11, board should be completely printed
+            break;
+        }
+    }       // end while loop  
+}
